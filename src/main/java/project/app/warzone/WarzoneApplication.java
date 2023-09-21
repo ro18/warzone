8215def2c7432d8f4289b45e4c4b7d38dc1cc457
ro18 <rochelle.cordiro30@gmail.com>
2023-09-20 23:46:17 -0400

    starter project

---
+++ src/main/java/project/app/warzone/WarzoneApplication.java
@@ -0,0 +1,13 @@
+package project.app.warzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarzoneApplication.class, args);
+	}
+
+}
