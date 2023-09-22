6d7c9b3db70ad74f1f453a5b8c011c3375c1ee75
PrashantPawar30 <145700893+PrashantPawar30@users.noreply.github.com>
2023-09-21 23:09:13 -0400

    added Player class

---
+++ src/main/java/project/app/warzone/Player/Player.java
@@ -0,0 +1,33 @@
+package project.app.warzone.Player;
+
+public class Player 
+{
+	
+		int l_playerid;
+		String l_playername;
+
+
+    void issue_order() {
+		}
+		
+		void next_order() {
+		}
+		
+		
+		public int getL_playerid() {
+			return l_playerid;
+		}
+		public void setL_playerid(int l_playerid) {
+			this.l_playerid = l_playerid;
+		}
+		public String getL_playername() {
+			return l_playername;
+		}
+		public void setL_playername(String l_playername) {
+			this.l_playername = l_playername;
+		}
+
+		
+		
+		
+}
