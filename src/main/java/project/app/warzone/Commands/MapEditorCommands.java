89b603eeb81b6122b06f7b5668cf087bff3c2bd8
Rochelle Cordeiro <41016437+ro18@users.noreply.github.com>
2023-09-23 23:20:52 -0400

    added new command

--- src/main/java/project/app/warzone/Commands/MapEditorCommands.java
+++ src/main/java/project/app/warzone/Commands/MapEditorCommands.java
@@ -7,6 +7,11 @@ import org.springframework.shell.standard.ShellMethod;
 public class MapEditorCommands {
 
 
+     @ShellMethod(key= "loadMap", value="Player can create or open an existing")
+    public String loadMap(@ShellOption String p_filename){
+        return "You can edit continents here"+p_filename;
+
+    }
     @ShellMethod(key= "editcontinent", value="This is used to add or update continents")
     public String editcontinent(){
         return "You can edit continents here";
