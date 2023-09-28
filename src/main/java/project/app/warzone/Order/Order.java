6aee8c4bd0ebecc20544eceb95d219e0284c0da2
Anash <anashshelat@gmail.com>
2023-09-29 01:46:28 +0530

    Added a Territory object

--- src/main/java/project/app/warzone/Order/Order.java
+++ src/main/java/project/app/warzone/Order/Order.java
@@ -1,16 +1,17 @@
 package project.app.warzone.Order;
 
+import project.app.warzone.Model.Territory;
 import project.app.warzone.Player.Player;
 
 public class Order {
     String d_orderType;
     int d_numberOfArmies;
     Player PlayerObject;
-    // Country CountryObject;
+    Territory Territory;
 
     void execute() {
         // Order will get execute here
     }
-
+    
 
 }
