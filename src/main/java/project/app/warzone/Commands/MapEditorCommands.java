43101e08882853775154f8cbfc8905231747c533
ro18 <rochelle.cordiro30@gmail.com>
2023-09-28 03:22:14 -0400

    changes for player

--- src/main/java/project/app/warzone/Commands/MapEditorCommands.java
+++ src/main/java/project/app/warzone/Commands/MapEditorCommands.java
@@ -3,7 +3,7 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 import project.app.warzone.Features.MapFeatures;
-import project.app.warzone.Model.Map;
+import project.app.warzone.Model.GameEngine;
 
 @ShellComponent
 public class MapEditorCommands {
@@ -11,12 +11,13 @@ public class MapEditorCommands {
    
     
     private final MapFeatures mapFeatures;
-    public Map map;
-//test
+    public GameEngine gameEngine;
+    public PlayerCommands playerCommands;
 
-    public MapEditorCommands(MapFeatures mapFeatures, Map map){
+    public MapEditorCommands(MapFeatures mapFeatures, GameEngine gameEngine){
         this.mapFeatures = mapFeatures;
-        this.map = map;
+        this.gameEngine = gameEngine;
+        playerCommands = new PlayerCommands(gameEngine);
 
 
     }
@@ -25,10 +26,10 @@ public class MapEditorCommands {
 
     @ShellMethod(key= "loadmap", value="Player can create or open an existing map")
     public String loadMap(@ShellOption String p_filename){
-        
-        if(map.fileExists(p_filename)){
+        gameEngine.prevUserCommand="loadmap";
+        if(gameEngine.gameMap.fileExists(p_filename)){
             System.out.println("One file found.");
-            map.set_USER_SELECTED_FILE(p_filename);
+            gameEngine.gameMap.set_USER_SELECTED_FILE(p_filename);
             return "Choose one of the below commands to proceed:\n 1. showmap 2.editmap";
         }
         else{
@@ -38,9 +39,9 @@ public class MapEditorCommands {
 
     @ShellMethod(key= "showmap", value="Used to display map continents with terriotories and boundaries")
     public void showmap(){
-        String p_mapLocation=map.getMapDirectory()+"\\"+map.get_USER_SELECTED_FILE()+".map";
+        String p_mapLocation=gameEngine.gameMap.getMapDirectory()+"\\"+gameEngine.gameMap.get_USER_SELECTED_FILE()+".map";
         //System.out.println("map location:"+p_mapLocation);
-        map = mapFeatures.readMap(p_mapLocation);
+        gameEngine.gameMap = mapFeatures.readMap(p_mapLocation);
 
     }
 
--- src/main/java/project/app/warzone/Commands/PlayerCommands.java
+++ src/main/java/project/app/warzone/Commands/PlayerCommands.java
@@ -3,25 +3,81 @@ package project.app.warzone.Commands;
     
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import project.app.warzone.Model.GameEngine;
 import project.app.warzone.Model.Player;
 import project.app.warzone.Model.Territory;
+
 
 @ShellComponent
 public class PlayerCommands {
 
+    public GameEngine gameEngine;
+    public String prevUserCommand; 
+
+    public PlayerCommands(GameEngine gameEngine){
+        this.gameEngine = gameEngine;
+    }
+
     @ShellMethod(key= "gameplayer", value="Player can create or remove a player")
-    public String gamePlayer(@ShellOption String p_playername){
+    public String gamePlayer(@ShellOption(value = "-add") String add,@ShellOption(value = "-r",defaultValue = ShellOption.NULL) String p_removePlayerCmd){
+
+        
+        return "work";
+        // String l_addCmd="-add";
+        // String l_removeCmd = "-remove";
+
+        // if(gameEngine.prevUserCommand.equals("loadmap")){
+        //     String l_playerCmd[] = p_playerCmd.split(" "); 
+
+        //     if(l_playerCmd[0].equals(l_addCmd)){
+        //         String l_playerName = l_playerCmd[1];
+        //         System.out.println("player name is : "+l_playerName);
+        //         List<Territory> listOfCountries = new ArrayList<>() ;
+        //         Player player= new Player(1, l_playerCmd[1],listOfCountries);
+        //         gameEngine.d_playersList.add(player);
+        //         return "Player added successfully";
+            
+        //     }
+        //     else if(l_playerCmd[0].equals(l_removeCmd)){
+
+        //         Optional<Player> playerToDelete = gameEngine.d_playersList.stream().filter(c-> c.getL_playername().equals(l_playerCmd[1])).findFirst();
+        //         if(playerToDelete!= null){
+        //             gameEngine.d_playersList.remove(playerToDelete);
+        //             return "Player deleted successfully";
+        //         }
+        //         else{
+        //             return "Player not found";
+        //         }
+                
+
+
+        //     }
+        //     else{
+        //     return "Invalid Command";
+        //     }
+
+        // }
+        // else{
+        //     return "You cannnot add players at this stage";
+        // }
+
         
 
-        List<Territory> listOfCountries = new ArrayList<>() ;
-        Player P = new Player(1, p_playername,listOfCountries);
-        return "String";
+        
     }
+
+    //  @ShellMethod(key= "gameplayer", value="Player can create or remove a player")
+    // public String gamePlayer(@ShellOption(value = "-remove") String p_playerCmd){
+    // }
+
+    
+
 
     // @ShellMethod(key= "gameplayer", value="Player can create or remove a player")
     // public String gamePlayer(@ShellOption String p_filename){
