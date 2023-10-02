d0927d6b7d2b7b4d34ce86035f0cf6e3d38e4729
Numan <numanslm99@gmail.com>
2023-10-02 19:01:02 -0400

    Created  mapFeaturesTest

---
+++ src/test/java/project/app/warzone/Features/mapFeaturesTest.java
@@ -0,0 +1,20 @@
+package Models;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Exceptions.InvalidMap;
+import Services.MapService;
+
+/**
+ * 
+ * This class is used to test functionality of Map class functions.
+ * 
+ */
+public class MapTest {
+  
+}
