77ec9be5861bfcb2051a6887e43e098f101a0117
PrashantPawar30 <pawarprashant172@gmail.com>
2023-10-03 01:24:06 -0400

    optimizing code

--- src/test/java/project/app/warzone/Features/PlayerFeaturesTest.java
+++ src/test/java/project/app/warzone/Features/PlayerFeaturesTest.java
@@ -16,29 +16,20 @@ public class PlayerFeaturesTest {
     public List<Player> playerList;
     public String playerName;
 
-    public PlayerFeatures playerFeatures;
+    public PlayerFeatures playerFeatures = new PlayerFeatures();;
     public GameEngine gameEngine;
 
     @Before
     public void setUp(){
-        playerFeatures = new PlayerFeatures();
         gameEngine = new GameEngine(gameEngine.getGameMap());
 
     }
 
     //1. successfully adding players
-
     @Test
     public void TestaddPlayers(){
-        //get player list
-        //assert statement to compare with returned number of players
-        //or check if the returned list is null 
-        //use assert null
-
-        playerFeatures.addPlayers("prashant", gameEngine);
-        playerFeatures.addPlayers("rochelle", gameEngine);
-        playerFeatures.addPlayers("aishwarya", gameEngine);
-        playerFeatures.addPlayers("anash", gameEngine);
+        
+        demoAddPlayers();
 
         String[] testPlayerNames = {"prashant","rochelle","aishwarya","anash"};
 
@@ -48,16 +39,8 @@ public class PlayerFeaturesTest {
     //2. successfully removing players
     @Test
     public void TestremovePlayers(GameEngine gameEngine, Player player){
-        //get player list before removing
-        //get player list after removing
-        //check if number is -1 or the name of removed player is in the list
-        //use assert and also compare list of player names
-
-        playerFeatures.addPlayers("prashant", gameEngine);
-        playerFeatures.addPlayers("rochelle", gameEngine);
-        playerFeatures.addPlayers("aishwarya", gameEngine);
-        playerFeatures.addPlayers("anash", gameEngine);
-
+        
+        demoAddPlayers();
         playerFeatures.removePlayers("prashant", gameEngine);
 
         String[] testPlayerNames = {"rochelle","aishwarya","anash"};
@@ -68,15 +51,8 @@ public class PlayerFeaturesTest {
     //3. check if all players are assigned countries and no player is missing
     @Test
     public void TestassignCountries(GameEngine gameEngine, Player player){
-        //get player list and assigned country list
-        //check if any player's country list is null 
-        //use assert null
-
-        playerFeatures.addPlayers("prashant", gameEngine);
-        playerFeatures.addPlayers("rochelle", gameEngine);
-        playerFeatures.addPlayers("aishwarya", gameEngine);
-        playerFeatures.addPlayers("anash", gameEngine);
-
+        
+        demoAddPlayers();
         playerFeatures.assignCountries(gameEngine);
 
         for( Player p : gameEngine.d_playersList)
@@ -88,15 +64,8 @@ public class PlayerFeaturesTest {
     //4. check if player is assigned reinforcements and no player is remaining to assign reinforcementpool
     @Test
     public void TestinitialReinforcement(GameEngine gameEngine, Player player){
-        //get player list and assigned reinforcement number
-        //check if any player's reinforcement list is null 
-        //use assert null
-
-        playerFeatures.addPlayers("prashant", gameEngine);
-        playerFeatures.addPlayers("rochelle", gameEngine);
-        playerFeatures.addPlayers("aishwarya", gameEngine);
-        playerFeatures.addPlayers("anash", gameEngine);
-
+        
+        demoAddPlayers();
         playerFeatures.assignCountries(gameEngine);
 
         for( Player p : gameEngine.d_playersList)
@@ -108,22 +77,21 @@ public class PlayerFeaturesTest {
     //5. player cannot deploy more armies that there is in their reinforcement pool. 
     @Test
     public void TestplayerDeploy(GameEngine gameEngine, Player player){
-
-        //add
-        //assign country
-        //deploy
-
-        playerFeatures.addPlayers("prashant", gameEngine);
-        playerFeatures.addPlayers("rochelle", gameEngine);
-        playerFeatures.addPlayers("aishwarya", gameEngine);
-        playerFeatures.addPlayers("anash", gameEngine);
-
+        
+        demoAddPlayers();
         playerFeatures.assignCountries(gameEngine);
 
         //try deploying larger army than possible should fail.
      //   playerFeatures.deployArmies(gameEngine, countryid, amry);
+      
 
+    }
 
-
+    void demoAddPlayers(){
+        
+        playerFeatures.addPlayers("prashant", gameEngine);
+        playerFeatures.addPlayers("rochelle", gameEngine);
+        playerFeatures.addPlayers("aishwarya", gameEngine);
+        playerFeatures.addPlayers("anash", gameEngine);
     }
 }
