0c027a83e0f6d529f0a95f2a72a2e913882dd1ce
Anash <anashshelat@gmail.com>
2023-10-05 05:46:02 +0530

    Bug fixing and code cleaning

--- src/main/java/project/app/warzone/Model/Node.java
+++ src/main/java/project/app/warzone/Model/Node.java
@@ -9,60 +9,50 @@ public class Node {
 
     private Country data;
 
-    private List<Node> borderNodes;
+    private List<Node> d_borderNodes;
 
     /**
      * counstructor for Node
      * 
-     * @param data          storing country data
+     * @param data storing country data
      */
     public Node(Country data) {
         this.data = data;
     }
 
-
-    
-    /** 
+    /**
      * @return Country
      */
     public Country getData() {
         return data;
     }
 
-
-    
-    /** 
+    /**
      * @return Country
      */
     public Country getDataPtr() {
         return data;
     }
 
-
-    
-    /** 
-     * @return                 returns bordernodes
+    /**
+     * @return returns bordernodes
      */
-    public List<Node> getBorders() { 
-        return borderNodes;
+    public List<Node> getBorders() {
+        return d_borderNodes;
     }
 
-
-    
-    /** 
-     * @param data                  set country data
+    /**
+     * @param data set country data
      */
     public void setData(Country data) {
         this.data = data;
     }
 
-
-    
-    /** 
-     * @param borderCountries       list of border countries
+    /**
+     * @param borderCountries list of border countries
      */
-    public void addBorderTerritories(List<Node> borderCountries){
-        borderNodes=borderCountries;
+    public void addBorderTerritories(List<Node> borderCountries) {
+        d_borderNodes = borderCountries;
     }
-    
+
 }
