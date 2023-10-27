3118cc58f758fb31b3b707ff7607f614749d9288
anash3420 <anashshelat@gmail.com>
2023-10-27 02:03:36 -0400

    Wrote file creation at game starttime logic

--- src/main/java/project/app/warzone/WarzoneApplication.java
+++ src/main/java/project/app/warzone/WarzoneApplication.java
@@ -1,5 +1,10 @@
 package project.app.warzone;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,6 +20,17 @@ public class WarzoneApplication {
 	 * @param args			string
 	 */
 	public static void main(String[] args) {
+
+		String l_currentDirectory = System.getProperty("user.dir") + "/src/main/java/project/app/warzone/gameplay.log";
+		File l_file = new File(l_currentDirectory);
+			try {
+				BufferedWriter l_writer = new BufferedWriter(new FileWriter(l_file, !(l_file.exists() && !l_file.isDirectory())));
+				l_writer.write("Game Commnece\n");
+				l_writer.close();
+			} catch (Exception e) {
+				System.out.println("Error Reading file");
+			}
+		
 		SpringApplication.run(WarzoneApplication.class, args);
 	}
 
