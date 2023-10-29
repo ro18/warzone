c0d9e0cf3dd92e299f090221e7d3869779524302
Numan <numanslm99@gmail.com>
2023-10-28 20:49:43 -0400

    Player feature states

--- src/main/java/project/app/warzone/Model/Play.java
+++ src/main/java/project/app/warzone/Model/Play.java
@@ -10,14 +10,32 @@
  */
 package project.app.warzone.Model;
 
+import project.app.warzone.Commands.MapEditorCommands;
+import project.app.warzone.Commands.PlayerCommands;
+
 public abstract class Play extends Phase {
+
+	public MapEditorCommands dMapEditorCommands;
+	public PlayerCommands dPlayerCommands;
+    Map dMap = new Map();
+    GameEngine ge = new GameEngine(dMapEditorCommands.returnMap());// map object is required to be passed here
+    String p_filename = dMap.get_USER_SELECTED_FILE();
 
 	Play(GameEngine p_ge) {
 		super(p_ge); 
 	}
 	
 	public void showMap() {
-		System.out.println("map is being displayed");		
+		System.out.println("G **************************************************************** Postload Shomap");
+
+        dMapEditorCommands.showmap();
+        System.out.println("Map is Dsiplayed");
+	}
+
+	public void showstats() {
+		System.out.println("G **************************************************************** Postload showstats");
+		dPlayerCommands.showStats();
+		System.out.println("Map is Dsiplayed");
 	}
 
 	public void editCountry() {
