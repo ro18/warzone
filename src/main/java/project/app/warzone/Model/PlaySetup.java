0fc48f9acb653f4c7712b8897231538de4834b79
Numan <numanslm99@gmail.com>
2023-10-28 20:49:43 -0400

    Player feature states

--- src/main/java/project/app/warzone/Model/PlaySetup.java
+++ src/main/java/project/app/warzone/Model/PlaySetup.java
@@ -1,6 +1,10 @@
 package project.app.warzone.Model;
+import project.app.warzone.Features.PlayerFeatures;
 
 public class PlaySetup extends Play {
+	public PlayerFeatures d_playerFeatures;
+	GameEngine ge = new GameEngine(dMapEditorCommands.returnMap());// map object is required to be passed here
+	String p_playerName = "player1";//add player import feature here
 
 	PlaySetup(GameEngine p_ge) {
 		super(p_ge);
@@ -11,10 +15,12 @@ public class PlaySetup extends Play {
 	}
 
 	public void setPlayers() {
+		d_playerFeatures.addPlayers( p_playerName, ge);
 		System.out.println("players have been set");
 	}
 
 	public void assignCountries() {
+		d_playerFeatures.assignCountries(ge);
 		System.out.println("countries have been assigned");
 	}
 
